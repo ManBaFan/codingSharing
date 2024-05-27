e3c67a75507f46c31abc423aa9abaec78fac0f4c
ethan <ethanfan92@gmail.com>
2024-05-27 21:26:23 +0800

    python learn

---
+++ PythonLearn/01-printing.py
@@ -0,0 +1,27 @@
+print("asd")
+print("sdf\nasdfa")
+print("sdfasdf")
+
+print("the", end=' ')
+print('big', end=' ')
+print("black", end=' ')
+print('box', end=' ')
+
+
+print('fsdff\nsfdsdf\nfsdf')
+print('''fsd
+      ffsdf
+      sdfsdf
+      fsdfsdf
+      ''')
+
+print("i am the 'manba',fan.")
+
+msg_2='manba loVe'
+print(msg_2)
+print(msg_2.title())
+print(msg_2.lower())
+print(msg_2.upper())
+
+msg_3 = 'nba'
+print( f"{msg_2.title()} {msg_3.upper()}")
