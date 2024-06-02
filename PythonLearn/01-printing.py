499377fb01e05a6e4eeb10e452654ca755674e36
ethan <ethanfan92@gmail.com>
2024-06-02 22:35:20 +0800

    update lab

--- PythonLearn/01-printing.py
+++ PythonLearn/01-printing.py
@@ -17,6 +17,7 @@ print('''fsd
 
 print("i am the 'manba',fan.")
 
+# practice string function
 msg_2='manba loVe'
 print(msg_2)
 print(msg_2.title())
@@ -25,3 +26,25 @@ print(msg_2.upper())
 
 msg_3 = 'nba'
 print( f"{msg_2.title()} {msg_3.upper()}")
+
+# practice null string, how to be strip
+msg_strip = ' ethanFan '
+print(msg_strip)
+print(msg_strip.lstrip())
+print(msg_strip.rstrip())
+print(msg_strip.strip())
+msg_2='manba loVe'
+print("\t\tEthanFan\n\txinGe \n\t"+ msg_2 + "\n\t\tjianhe")
+
+1+1
+1.0+1
+(1+2)*3
+2**2*3
+2*2**3
+2*2**(3*(3+3))
+3*0.13
+0.2+0.1
+0.1*0.2
+4/2
+1+2.00
+print(1000_000_000.00_00)
