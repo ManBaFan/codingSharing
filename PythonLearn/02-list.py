e5f66b0857be2627544272f39affd02f0029603b
ethan <ethanfan92@gmail.com>
2024-06-22 17:47:52 +0800

    update machine learning

--- PythonLearn/02-list.py
+++ PythonLearn/02-list.py
@@ -7,9 +7,15 @@ bicycles=["jeante","trick",'cannondale']
 print(bicycles[0])
 print(bicycles[1])
 
-# add elements
+# add elements,append
 bicycles=["jeante","trick",'cannondale']
 bicycles.append("meilida")
+print(bicycles)
+
+# insert
+bicycles=["jeante","trick",'cannondale']
+bicycles.insert(1,'manba')
+bicycles.insert(0,'meilida')
 print(bicycles)
 
 # delete elements, (1)use del 
@@ -33,3 +39,30 @@ print(bicycles)
 bicycles=["jeante","trick",'cannondale','others','trick']
 bicy_give=bicycles.remove('trick')
 print(bicycles)
+
+# update
+bicycles=["jeante","trick",'cannondale','others','trick']
+bicycles[1]='candy'
+print(bicycles)
+
+# manage list sort
+bicycles=["jeante","trick",'cannondale','others','trick']
+bicycles.sort(reverse=True)
+print(bicycles)
+
+# manage list sorted
+bicycles=["jeante","trick",'cannondale','others','trick']
+print(f'temp is {sorted(bicycles,reverse=True)}')
+print(bicycles)
+
+# manage list reverse
+bicycles=["jeante","trick",'cannondale','others','trick']
+print(f'original series is {bicycles}')
+bicycles.reverse()
+print(f'now is {bicycles}')
+bicycles.reverse()
+print(f'final is {bicycles}')
+
+# manage list reverse
+bicycles=["jeante","trick",'cannondale','others','trick']
+print(len(bicycles))
