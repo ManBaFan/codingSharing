95b0dd4c0520bcb48e71d2b8676779de515a62a4
ethan <ethanfan92@gmail.com>
2024-08-06 17:45:54 +0800

    update python learn dict

--- PythonLearn/04-dic.py
+++ PythonLearn/04-dic.py
@@ -41,3 +41,42 @@ for key in per_dic.keys():
 # item value
 for value in per_dic.values():
     print(value.lower())
+
+print(list(range(30)))
+print(set(range(30)))
+
+
+# list embed dict
+alion1 = {1:1,2:2}
+alion2 = {21:21,22:22}
+list_dict = [alion1,alion2]
+# print(list_dict)
+for alion in list_dict:
+    print(f"the dict is {alion}")
+
+# dict embed alion
+alion3 = {
+    'ethan':['c','python','go'],
+    'dave':['c'],
+    'kobe':['ruby','c++']
+}
+for alion in alion3.keys():
+    print(f'{alion} favirote language is:')
+    for la in alion3[alion]:
+        print(f'\t{la}')
+
+# dict embed dict, inner dict item type is same
+alion4 = {
+    'ethan':{'age':32,'gender':'male','height':172},
+    'dave':{'age':42,'gender':'female','height':170}
+}
+
+for user,user_info in alion4.items():
+    print(f'This is {user.title()} information:')
+    print(f'\tthe age is {user_info['age']}\n\tthe gender is {user_info['gender'].title()}\n\tthe height is {user_info['height']}')
+
+# for i in alion4:
+#     print(f'This is {i.title()} information: ')
+#     print(f'\tthe age is {alion4[i].get('age')}')
+#     print(f'\tthe gender is {alion4[i].get('gender').title()}')
+#     print(f'\tthe height is {alion4[i].get('height')}')
