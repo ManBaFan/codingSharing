ff145ace700d2c3400d46349e63b6ef1470506df
Ethan Bryant <ethanfan92@gmail.com>
2025-04-07 19:38:20 +0800

    Add class profile learn

--- PythonLearn/07-class_learn.py
+++ PythonLearn/07-class_learn.py
@@ -12,3 +12,57 @@ class Dog:
     
 dog = Dog('trump','13')
 dog.sit()
+
+
+class Car:
+    '''modify class profile'''
+    def __init__(self,name,model,year):
+        self.name=name
+        self.model=model
+        self.year=year
+        self.mile=0
+    
+    def desc_name(self):
+        '''print total name of car'''
+        long_name=f'{self.name},{self.model},{self.year}'
+        return long_name
+    
+    def read_mile(self):
+        '''print miles'''
+        print(f'The car mile is {self.mile}')
+
+    def update_mile(self,mileage):
+        '''modify mile'''
+        if(mileage>self.mile):
+            self.mile=mileage
+        else:
+            print('You can\'t roll back mile.')
+    
+    def incre_mile(self,add_miles):
+        '''incredible miles'''
+        if(add_miles>=0):
+            self.mile+=add_miles
+        else:
+            print('You can\'t add negative num.')
+
+my_car=Car('sky','vechiel',2019)
+print(my_car.desc_name())
+my_car.read_mile()
+# direct modify profile
+my_car.mile=2025
+print(my_car.mile)
+
+# modify by func
+my_car.update_mile(202504)
+my_car.read_mile()
+
+# modify by func
+my_car.update_mile(202503)
+my_car.read_mile()
+
+# incredible negative miles
+my_car.incre_mile(-10)
+my_car.read_mile()
+# incredible negative miles
+my_car.incre_mile(10)
+my_car.read_mile()
